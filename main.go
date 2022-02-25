017dc72b7dd444bc4eb12f9d14e152d1ecc3db6a
C1325543 Matheus Romano Braga <romanobraga.m@gmail.com>
2022-02-25 14:53:51 -0300

    fix: linter error

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const DefaultKnowledgeBaseName = "default"
 // DefaultKnowledgeBaseVersion its default version of Knowledge Base
 const DefaultKnowledgeBaseVersion = "latest"
 
-// my var Config is ...
+//Config ...
 var Config = config.Config{}
 
 func main() {
