9e94af1860dc2e279a17009078462c1178b0ad5f
C1325543 Matheus Romano Braga <romanobraga.m@gmail.com>
2022-03-11 15:20:54 -0300

    feat: implementing new unit tests on typedMap file

--- types/typedMap_test.go
+++ types/typedMap_test.go
@@ -18,6 +18,15 @@ func TestNewTypedMap(t *testing.T) {
 
 }
 
+func TestNewTypedMapFromMap(t *testing.T) {
+	m := interfaceMap{"mystring": "test"}
+	got := NewTypedMapFromMap(m).GetEntries()
+	expected := m
+	if reflect.DeepEqual(got, expected) {
+		t.Error("You got an error while try to generate a new typed map from a map")
+	}
+}
+
 func TestPut(t *testing.T) {
 	tm := NewTypedMap()
 	tm.Put("mystring", "test")
@@ -28,7 +37,6 @@ func TestPut(t *testing.T) {
 	if got != expected {
 		t.Error("You got an error while try to put value into map")
 	}
-
 }
 
 func TestHasTrue(t *testing.T) {
@@ -65,6 +73,43 @@ func TestGet(t *testing.T) {
 	got := tm.Get(param)
 
 	expected := "test"
+
+	if got != expected {
+		t.Error("Test fail, the param doesn't exists into map")
+	}
+}
+
+func TestGetEntry(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("mystring", "test")
+	param := "mystring"
+
+	got := tm.GetEntry(param)
+
+	expected := "test"
+
+	if got != expected {
+		t.Error("Test fail, the param doesn't exists into map")
+	}
+}
+
+type mockGetter struct {
+	Getter
+}
+
+func (m *mockGetter) GetEntry(param string) interface{} {
+	return "mock"
+}
+
+func TestGetWithGetter(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Getter = &mockGetter{}
+	tm.Put("mystring", "test")
+	param := "mystrifdfdng"
+
+	got := tm.Get(param)
+
+	expected := "mock"
 
 	if got != expected {
 		t.Error("Test fail, the param doesn't exists into map")
@@ -151,6 +196,73 @@ func TestGetIntWithPanic(t *testing.T) {
 	tm.GetInt("myint")
 }
 
+func TestGetFloatWithNoParams(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("myint", "")
+	got := tm.GetFloat("")
+	expected := float64(0)
+
+	if got != expected {
+		t.Error("Couldn't get the float value into the map")
+	}
+}
+func TestGetFloatWithString(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("myFloatString", "5.5")
+	got := tm.GetFloat("myFloatString")
+	expected := float64(5.5)
+
+	if got != expected {
+		t.Error("Couldn't get the string into the map")
+	}
+}
+
+func TestGetFloatWithInteger(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("myFloatInteger", 1)
+	got := tm.GetFloat("myFloatInteger")
+	expected := float64(1.0)
+
+	if got != expected {
+		t.Error("Couldn't get the integer into the map")
+	}
+}
+
+func TestGetFloatWithInt64(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("myFloatInt64", int64(12))
+	got := tm.GetFloat("myFloatInt64")
+	expected := float64(12.0)
+
+	if got != expected {
+		t.Error("Couldn't get the int64 into the map")
+	}
+}
+
+func TestGetFloatWithFloat64(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("myFloat64", float64(12.562))
+	got := tm.GetFloat("myFloat64")
+	expected := float64(12.562)
+
+	if got != expected {
+		t.Error("Couldn't get the float64 into the map")
+	}
+}
+
+func TestGetFloatWithPanic(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != "fail to retrieve this param as float64" {
+			t.Error("The panic message it's not throwed")
+		}
+	}()
+	tm := NewTypedMap()
+	tm.Put("myWrongParam", false)
+	tm.GetFloat("myWrongParam")
+
+}
+
 func TestGetBool(t *testing.T) {
 	tm := NewTypedMap()
 	tm.Put("mybool", "true")
@@ -160,6 +272,50 @@ func TestGetBool(t *testing.T) {
 	if got != expect {
 		t.Error("Couldn't get the bool value into the map")
 	}
+
+}
+
+func TestGetMap(t *testing.T) {
+	tm := NewTypedMap()
+	mymap := map[string]interface{}{
+		"mystring": "test",
+		"myint":    15,
+		"mybool":   true,
+	}
+
+	tm.Put("mymap", mymap)
+
+	got := tm.GetMap("mymap").GetEntries()
+	expected := mymap
+
+	if reflect.DeepEqual(got, expected) != true {
+		t.Error("The maps aren't equals")
+	}
+
+}
+
+func TestGetMapNil(t *testing.T) {
+	tm := NewTypedMap()
+	tm.Put("mymap", "")
+
+	got := tm.GetMap("")
+
+	if got != nil {
+		t.Error("The maps aren't equals")
+	}
+
+}
+
+func TestGetMapWithoutMap(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r != "This param it's not a map" {
+			t.Error("The panic message it's not throwed")
+		}
+	}()
+	tm := NewTypedMap()
+	tm.Put("mymap", "test")
+	tm.GetMap("mymap")
 
 }
 
