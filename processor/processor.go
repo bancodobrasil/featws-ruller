2afc5dd4060b12a1180ac4fd126dea33a5ee781d
Raphael Pinto <raphaelcpinto@gmail.com>
2022-05-10 23:51:47 -0300

    fix: lint

--- processor/processor.go
+++ processor/processor.go
@@ -28,6 +28,7 @@ func (p *Processor) Contains(slice []interface{}, val interface{}) bool {
 	return false
 }
 
+// ToMap method to convert a string to a map
 func (p *Processor) ToMap(objectstring string) map[string]interface{} {
 	o := make(map[string]interface{})
 
