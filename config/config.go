12c5c3fdff485552b52084d69d79f059dd9c56bd
Mayco Lacerda <maycolacerda@gmail.com>
2023-06-21 13:06:19 -0300

    fix: solving merge conflicts

--- config/config.go
+++ config/config.go
@@ -10,7 +10,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-<<<<<<< HEAD
 // Config type contains various configuration options for a program, including resource loader
 // settings, port, default rules, SSL verification, resolver bridge settings, external host, and API
 // key.
@@ -28,9 +27,7 @@ import (
 //   - ResolverBridgeHeadersStr: This property is a string representation of the HTTP headers that will be sent to the resolver bridge. It is used in conjunction with the ResolverBridgeHeaders property to set the headers for requests made to the resolver bridge. The headers can be specified as a JSON object in string format.
 //   - ExternalHost: This property represents the external host name or IP address of the server where the application is running. It is used to construct URLs for external resources and APIs.
 //   - AuthAPIKey: This property is used to store the API key for authentication purposes. It is used to authenticate requests made to the Ruller API.
-=======
-// Config ...
->>>>>>> cache-ruller
+
 type Config struct {
 	ResourceLoaderType       string `mapstructure:"FEATWS_RULLER_RESOURCE_LOADER_TYPE"`
 	ResourceLoaderURL        string `mapstructure:"FEATWS_RULLER_RESOURCE_LOADER_URL"`
